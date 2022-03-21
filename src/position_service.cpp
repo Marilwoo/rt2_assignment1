cc9f62b54ebf7ac363aced7d84f211b3b2d9b525
Marilwoo <74102105+Marilwoo@users.noreply.github.com>
2022-03-21 10:15:06 +0100

    Update position_service.cpp

--- src/position_service.cpp
+++ src/position_service.cpp
@@ -1,18 +1,17 @@
 #include "ros/ros.h"
 #include "rt2_assignment1/RandomPosition.h"
 
-
+// Computing the random between two numbers
 double randMToN(double M, double N)
 {     return M + (rand() / ( RAND_MAX / (N-M) ) ) ; }
 
-
+// Asigning the random number to the response of the service
 bool myrandom (rt2_assignment1::RandomPosition::Request &req, rt2_assignment1::RandomPosition::Response &res){
     res.x = randMToN(req.x_min, req.x_max);
     res.y = randMToN(req.y_min, req.y_max);
     res.theta = randMToN(-3.14, 3.14);
     return true;
 }
-
 
 int main(int argc, char **argv)
 {
