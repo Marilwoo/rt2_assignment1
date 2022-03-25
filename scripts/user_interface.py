6e6e290d3a3656f6fec763bb2b2a2f3915a8e201
Marilwoo <74102105+Marilwoo@users.noreply.github.com>
2022-03-25 16:51:00 +0100

    Update user_interface.py

--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -2,13 +2,16 @@ import rospy
 import time
 from rt2_assignment1.srv import Command
 
+# Interface for getting the user inputs
 def main():
     rospy.init_node('user_interface')
     ui_client = rospy.ServiceProxy('/user_interface', Command)
     time.sleep(10)
     rate = rospy.Rate(20)
+    # Getting the input and assigning to the x variable
     x = int(input("\nPress 1 to start the robot "))
     while not rospy.is_shutdown():
+        # Checking the x value and sending it to the user interface server
         if (x == 1):
             ui_client("start")
             x = int(input("\nPress 0 to stop the robot "))
